badd073d9af58fa7e4d994af277459f71fd13402
wangsongqing <xmwme@qq.com>
2025-08-06 18:09:50 +0800

    vue setup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script lang="ts">
-import Person from './components/Person.vue'
-import ShowModel from './components/ShowModel.vue';
+// import Person from './components/Person.vue'
+// import ShowModel from './components/ShowModel.vue';
+import PersonOne from './components/PersonOne.vue';
 export default {
   name: 'App',
-  components: { Person, ShowModel }
+  components: { PersonOne }
 }
 </script>
 
 <template>
-  <Person></Person>
-  <ShowModel></ShowModel>
+  <!-- <Person></Person> -->
+  <!-- <ShowModel></ShowModel> -->
+  <PersonOne></PersonOne>
 </template>
 
 <style></style>
---
+++ src/components/PersonOne.vue
@@ -0,0 +1,34 @@
+<template>
+  <p>姓名:{{ name }}</p>
+  <p>年龄:{{ age }}</p>
+  <button @click="showTel">查看电话</button>
+  <button @click="changeName">修改名字</button>
+  <button @click="addAge">增加年龄</button>
+</template>
+
+<script setup>
+let name = 'wsq'
+let age = 12
+let tel = '18200000001'
+
+function showTel() {
+  alert(tel)
+}
+
+function changeName() {
+  name = '三个'
+  console.log(name)
+}
+
+function addAge() {
+  age++
+  console.log(age)
+}
+
+</script>
+
+<style scoped>
+button {
+  margin: 0 10px;
+}
+</style>
